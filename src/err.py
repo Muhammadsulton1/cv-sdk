454326c1c1d420e56151f7ba5851f2b1f35c97c2
Sulton <SodikovMAg@nornik.ru>
2025-07-23 22:25:21 +0300

    Добавил примерное описание архитектуры

--- src/err.py
+++
@@ -1,5 +0,0 @@
-class FrameDecodeError(Exception):
-    """Ошибка декодирования кадра"""
-    def __init__(self, message="Ошибка декодирования кадра"):
-        super().__init__(message)
-        self.message = message
